8e091b9d4b5f5cc79b5317db78bbf4c0e10bd4b4
1KitCat1 <denischool119@gmail.com>
2022-06-11 19:36:09 +0300

    Created header for authorized page

--- gloden_peach/pages/dashboard.js
+++ gloden_peach/pages/dashboard.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import Header from '../components/header'
 
 const Dashboard = ({address}) => {
-    return <div>{address}</div>
+    return <Header/>
 }
 
 export default Dashboard;
