78f2e6cdef05fae8811d75051087cc2aaa8a9d57
1KitCat1 <denischool119@gmail.com>
2022-06-11 19:53:47 +0300

    Created buy-sell and send-receive buttons

--- gloden_peach/components/header.js
+++ gloden_peach/components/header.js
@@ -3,18 +3,47 @@ import styled from "styled-components"
 
 const Header = () => {
     return (
-        <Button></Button>
+        <Wrapper>
+            <ButtonBlue>Buy | Sell</ButtonBlue>
+            <Button>Send | Receive</Button>
+        </Wrapper>
+        
     )
 }
 export default Header
 
+const Wrapper = styled.div`
+  width: calc(100%);
+  padding: 1rem 1.5rem;
+  border-bottom: 1px solid #282b2f;
+  background-color: #1a1b1d;
+  display: flex;
+  align-items: center;
+`
+
 const Button = styled.div`
+  border: 3px solid #282b2f;
+  background-color: #262526;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  margin-right: 1rem;
+  color: white;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const ButtonBlue = styled.div`
   border: 1px solid #282b2f;
   padding: 0.8rem;
   font-size: 1.3rem;
   font-weight: 500;
   border-radius: 0.4rem;
   margin-right: 1rem;
+  background-color: #3773f5;
+  color: black;
   &:hover {
     cursor: pointer;
   }
