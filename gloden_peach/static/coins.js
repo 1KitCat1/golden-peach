36cda77f6e677eb66bdf875108650d193246a698
Daniel <danya.ostap@gmail.com>
2022-06-11 20:59:24 +0300

    Created coins.js to display assets

---
+++ gloden_peach/static/coins.js
@@ -0,0 +1,36 @@
+import btcLogo from '../crypto_assets/bitcoin-btc-logo.png'
+import ethLogo from '../crypto_assets/ethereum-eth-logo.png'
+import nearLogo from '../crypto_assets/near-protocol-near-logo.png'
+
+export const coins = [
+  {
+    name: 'Bitcoin',
+    sign: 'BTC',
+    logo: btcLogo,
+    balanceUsd: 230.32,
+    balanceCoin: 6.35667736,
+    priceUsd: 5.32,
+    change: -4.74,
+    allocation: 41.89,
+  },
+  {
+    name: 'Ethereum',
+    sign: 'ETH',
+    logo: ethLogo,
+    balanceUsd: 1.2,
+    balanceCoin: 3.35664236,
+    priceUsd: 5.22,
+    change: 6.24,
+    allocation: 17.89,
+  },
+  {
+    name: 'NEAR Protocol',
+    sign: 'NEAR',
+    logo: nearLogo,
+    balanceUsd: 1.2,
+    balanceCoin: 3.35664236,
+    priceUsd: 5.22,
+    change: -5.24,
+    allocation: 17.89,
+  },
+]
