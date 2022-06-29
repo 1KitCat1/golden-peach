3edc34f7e1cb0bb8e9878ac8b0f6ff86e188f76c
1KitCat1 <denischool119@gmail.com>
2022-06-29 23:54:36 +0300

    Added page routing

--- gloden_peach/pages/dashboard.js
+++ gloden_peach/pages/dashboard.js
@@ -3,14 +3,25 @@ import Header from '../components/header'
 import styled from 'styled-components'
 import Main from '../components/main'
 import Sidebar from '../components/sidebar'
+import InviteFriends from '../components/inviteFriends'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const getContent = (router, address) => {
+    if(router.query.invite){
+        return <InviteFriends walletAddress={address}/>
+    }
+    return <Main/>
+}
 
 const Dashboard = ({address}) => {
+    const router = useRouter()
     return (
     <Wrapper>
         <Sidebar walletAddress={address}/>
         <MainContainer>
             <Header walletAddress={address}/>
-            <Main/>
+            {getContent(router, address)}
         </MainContainer>
     </Wrapper>
     )
