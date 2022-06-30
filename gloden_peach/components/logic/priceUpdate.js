dfab4ec5056939827c576be91daa3323728bb161
1KitCat1 <denischool119@gmail.com>
2022-06-30 18:31:52 +0300

    Added currency price updating

--- gloden_peach/components/logic/priceUpdate.js
+++ gloden_peach/components/logic/priceUpdate.js
@@ -1,7 +1,22 @@
 import { coins } from "../../static/coins"
 const PriceUpdate = () => {
-    coins[0].priceUsd = 300;
-    console.log()
+    for(let i = 0; i < coins.length; i += 1){
+        if(!coins[i].name) continue;
+        let name = coins[i].name.split(" ")[0].toLocaleLowerCase();
+        const Http = new XMLHttpRequest();
+        let request = "https://api.coingecko.com/api/v3/simple/price?ids=" 
+            + name 
+            + "&vs_currencies=usd";
+        Http.open("GET", request);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            let price = Http.responseText.toString();
+            if(price){
+                price = (price.split(':')[2]).split('}')[0]
+                coins[i].priceUsd = Number(price);
+            }
+        }   
+    }
 }
 
 export default PriceUpdate
