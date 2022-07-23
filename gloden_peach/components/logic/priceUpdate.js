f9ed7613a3dc4575d1d031671a7f70cb6f53b9f7
1KitCat1 <denischool119@gmail.com>
2022-07-23 22:06:24 +0300

    Added price changing percentage

--- gloden_peach/components/logic/priceUpdate.js
+++ gloden_peach/components/logic/priceUpdate.js
@@ -8,14 +8,17 @@ const PriceUpdate = () => {
         const Http = new XMLHttpRequest();
         let request = "https://api.coingecko.com/api/v3/simple/price?ids=" 
             + name 
-            + "&vs_currencies=usd";
+            + "&vs_currencies=usd&include_24hr_change=true";
         Http.open("GET", request);
         Http.send();
         Http.onreadystatechange = (e) => {
-            let price = Http.responseText.toString();
-            if(price){
-                price = (price.split(':')[2]).split('}')[0]
-                coins[i].priceUsd = Number(price);
+            let responseData = Http.responseText.toString();
+            if(responseData){
+                // price = (price.split(':')[2]).split('}')[0]
+                // coins[i].priceUsd = Number(price);
+                let parsedData = JSON.parse(responseData);
+                coins[i].priceUsd = Number(parsedData[name]["usd"]).toFixed(2);
+                coins[i].change = Number(parsedData[name]["usd_24h_change"]).toFixed(2);
             }
         }   
     }
